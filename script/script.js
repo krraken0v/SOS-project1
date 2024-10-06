61445e6f5a5b9bd194c9c3c6623014271acc494b
unknown <kohanickola@gmail.com>
2024-10-06 19:28:15 +0200

    slider-section-almost-done

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
 window.addEventListener("DOMContentLoaded",function(){
-    const languageList = document.querySelector(".language");
-    const englishLanguage = document.querySelector(".siteEnglish");
-    const russishLanguage = document.querySelector(".siteRussish");
+    const languageList = document.querySelector(".language"),
+    englishLanguage = document.querySelector(".siteEnglish"),
+    russishLanguage = document.querySelector(".siteRussish"),
+    slides = document.querySelectorAll(".about_slides img"),
+    buttonSwitchSlides = document.querySelector(".about_slides_switch");
+    console.log(slides);
     languageList.addEventListener("change",function(){
         if(this.value=="value2"){
             this.value == "value2";
@@ -16,4 +19,23 @@ window.addEventListener("DOMContentLoaded",function(){
             englishLanguage.classList.add("show");
         }
     })
+    let current =0;
+    function showSlides(){
+        for(let i=0;i<slides.length;i++){
+            slides[i].classList.add("hide");
+            slides[i].classList.remove("show");
+        }
+        slides[current].classList.remove("hide");
+        slides[current].classList.add("show");
+    }
+    showSlides();
+    buttonSwitchSlides.addEventListener("click",function(){
+        if(current==slides.length-1){
+            current=0;
+        } else{
+            current++;
+        }
+        showSlides();
+    })
+   
 })
